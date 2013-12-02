39310373a6f1924b1682e79f26cf64375873c5bf
Mike Therien <mike@miketherien.com>
2013-12-01 22:00:25 -0500

    Added support for path mapping

--- src/TFSReplay/Program.cs
+++ src/TFSReplay/Program.cs
@@ -14,6 +14,7 @@ namespace TFSReplay
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
             var options = new TFSReplayOptions();
@@ -42,93 +43,59 @@ namespace TFSReplay
             }
             
             var serverUri = new Uri(options.ServerUrl);
-            var tpc = new TfsTeamProjectCollection(serverUri);
-            tpc.EnsureAuthenticated();
+            var sourceTeamProjectCollection = new TfsTeamProjectCollection(serverUri);
+            sourceTeamProjectCollection.EnsureAuthenticated();
+            var sourceVersionControlService = sourceTeamProjectCollection.GetService<VersionControlServer>();
 
-            var vcs = tpc.GetService<VersionControlServer>();
-            var destWorkspace = vcs.GetWorkspace(destinationFolder.FullName);
-            
-            var changeset = vcs.GetChangeset(changesetId);
+            TfsTeamProjectCollection destinationTeamProjectCollection = sourceTeamProjectCollection;
+            Workspace destinationWorkspace = null;
+            //if (!string.IsNullOrWhiteSpace(options.DestinationUrl))
+            //{
+            //    destinationTeamProjectCollection = new TfsTeamProjectCollection(new Uri(options.DestinationUrl));
+            //    destinationTeamProjectCollection.EnsureAuthenticated();
+            //    var destinationVersionControlService =
+            //        destinationTeamProjectCollection.GetService<VersionControlServer>();
+            //    destinationWorkspace = destinationVersionControlService.GetWorkspace(destinationFolder.FullName);
+            //}
 
-            Console.WriteLine("Changeset {0}, committed by {1}:", changesetId,changeset.CommitterDisplayName);
-            Console.WriteLine(changeset.Comment);
-            Console.WriteLine();
-
-            var changedFiles = from change in changeset.Changes
-                               where
-                                   ((change.ChangeType & ChangeType.Edit) == ChangeType.Edit
-                                   || (change.ChangeType & ChangeType.Add) == ChangeType.Add
-                                   || (change.ChangeType & ChangeType.Delete) == ChangeType.Delete)
-                               select change;
-
-            var changedItems = new List<string>();
-
-            foreach (var changedFile in changedFiles)
+            if (destinationWorkspace == null)
             {
-                var serverFilePath = changedFile.Item.ServerItem;
-                var strippedPath = serverFilePath.Replace(options.BaseServerPath, "");
-                strippedPath = strippedPath.Replace('/', '\\');
-                strippedPath = strippedPath.TrimStart('\\');
-                
-
-                Console.WriteLine("File: {0}", strippedPath);
-
-
-                var isEdit = (changedFile.ChangeType & ChangeType.Edit) == ChangeType.Edit;
-                var isAdd = (changedFile.ChangeType & ChangeType.Add) == ChangeType.Add;
-                var isDelete = (changedFile.ChangeType & ChangeType.Delete) == ChangeType.Delete;
-
-                var newDestination = destinationFolder.FullName + "\\" + strippedPath;
-
-                var fileExists = File.Exists(newDestination);
-
-                if (isAdd || (isEdit && !fileExists))
+                try
                 {
-                    // Add file
-                    if (changedFile.Item.ItemType == ItemType.Folder)
-                    {
-                        Directory.CreateDirectory(newDestination);
-                    }
-                    else
-                    {
-                        changedFile.Item.DownloadFile(newDestination);
-                    }
-                    destWorkspace.PendAdd(newDestination);
-                    Console.WriteLine("+{0}", MakeRelativePath(startupFolder.FullName, newDestination));
-                    changedItems.Add(newDestination);
+                    destinationWorkspace = sourceVersionControlService.GetWorkspace(destinationFolder.FullName);
                 }
-                else if (isEdit)
+                catch (Exception ex)
                 {
-                    if (changedFile.Item.ItemType != ItemType.Folder)
-                    {
-                        destWorkspace.PendEdit(newDestination);
-
-                        // Replace file
-                        changedFile.Item.DownloadFile(newDestination);
-                        Console.WriteLine("~{0}", MakeRelativePath(startupFolder.FullName, newDestination));
-                        changedItems.Add(newDestination);
-                        
-                    }
-                }
-                else if (isDelete)
-                {
-                    // Delete file
-                    if (fileExists)
-                    {
-                        destWorkspace.PendDelete(newDestination);
-                        Console.WriteLine("-{0}", MakeRelativePath(startupFolder.FullName, newDestination));
-                        changedItems.Add(newDestination);
-                    }
+                    if (ex.GetType().Name != "ItemNotMappedException")
+                        throw;
                 }
                 
-
             }
+
+            var mapPaths = new Dictionary<string, string>();
+            if (!string.IsNullOrEmpty(options.MapFile))
+            {
+                mapPaths = LoadMapFile(options.MapFile);
+            }
+
+            var changeset = sourceVersionControlService.GetChangeset(changesetId);
+            var logger = new ConsoleLogger();
+
+            var changesetProcessor = new ChangesetProcessor(destinationWorkspace, logger);
+
+            var changedItems = changesetProcessor.ProcessChangeset(changeset, destinationFolder, startupFolder, options.BaseServerPath, mapPaths);
 
             Console.WriteLine();
             Console.WriteLine();
 
             if (options.Checkin)
             {
+                if (changedItems.Count <= 0)
+                {
+                    Console.WriteLine("*** No changes detected, a checkin is not necessary.");
+                    return;
+                }
+
                 var newCommentFormat =
                     "Original changeset {0}, committed by {1}, was moved to this location.\r\n\r\nOriginal comment:\r\n{2}";
 
@@ -136,10 +103,25 @@ namespace TFSReplay
                     changeset.Comment);
 
                 var changeList = new StringBuilder();
-                var pendingChanges = destWorkspace.GetPendingChanges(changedItems.ToArray());
+                PendingChange[] pendingChanges = new PendingChange[0];
+                if (destinationWorkspace != null) 
+                {
+                    pendingChanges = destinationWorkspace.GetPendingChanges(changedItems.ToArray());
+                }
+
                 foreach (var pendingChange in pendingChanges)
                 {
-                    changeList.AppendLine(string.Format("{0}: {1}", pendingChange.ChangeTypeName, MakeRelativePath(startupFolder.FullName, pendingChange.LocalItem)));
+                    changeList.AppendLine(string.Format("{0}: {1}", pendingChange.ChangeTypeName, PathHelper.MakeRelativePath(startupFolder.FullName, pendingChange.LocalItem)));
+                }
+
+                {
+                    var currentClipboard = System.Windows.Forms.Clipboard.GetText();
+                    if (!string.IsNullOrWhiteSpace(currentClipboard))
+                    {
+                        currentClipboard +=
+                            "\r\n--------------------------------------------------------------------------\r\n";
+                    }
+                    System.Windows.Forms.Clipboard.SetText(currentClipboard + newComment);
                 }
 
                 if (pendingChanges.Length == 0)
@@ -149,8 +131,14 @@ namespace TFSReplay
                 }
 
                 bool doCheckin = false;
-                if (!options.NoPrompt)
+                if (logger.HasErrors || !options.NoPrompt)
                 {
+                    if (logger.HasErrors)
+                    {
+                        Console.WriteLine("*** WARNING *** There were warnings in processing the changeset.");
+                        Console.WriteLine("Please look at the output and confirm you want to checkin the changes.");
+                        Console.WriteLine();
+                    }
                     Console.WriteLine("About to commit:");
                     Console.WriteLine(changeList);
                     Console.WriteLine();
@@ -172,40 +160,47 @@ namespace TFSReplay
 
                 if (doCheckin)
                 {
-                    var newChangeset = destWorkspace.CheckIn(pendingChanges, newComment);
-                    Console.WriteLine();
-                    Console.WriteLine("Checked in, changeset {0}", newChangeset);
+                    if (destinationWorkspace == null)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Destination Workspace undetermined.  Unable to checkin");
+                    }
+                    else
+                    {
+                        var newChangeset = destinationWorkspace.CheckIn(pendingChanges, newComment);
+                        Console.WriteLine();
+                        Console.WriteLine("Checked in, changeset {0}", newChangeset);
+                    }
                 }
+
             }
             
         }
 
-        /// <summary>
-        /// Creates a relative path from one file or folder to another.
-        /// </summary>
-        /// <param name="fromPath">Contains the directory that defines the start of the relative path.</param>
-        /// <param name="toPath">Contains the path that defines the endpoint of the relative path.</param>
-        /// <param name="dontEscape">Boolean indicating whether to add uri safe escapes to the relative path</param>
-        /// <returns>The relative path from the start directory to the end path.</returns>
-        /// <exception cref="ArgumentNullException"></exception>
-        public static String MakeRelativePath(String fromPath, String toPath)
+        private static Dictionary<string,string> LoadMapFile(string mapFile)
         {
-            if (String.IsNullOrEmpty(fromPath)) throw new ArgumentNullException("fromPath");
-            if (String.IsNullOrEmpty(toPath)) throw new ArgumentNullException("toPath");
+            var map = new Dictionary<string, string>();
 
-            Uri fromUri = new Uri(fromPath);
-            Uri toUri = new Uri(toPath);
-
-            Uri relativeUri = fromUri.MakeRelativeUri(toUri);
-            String relativePath = Uri.UnescapeDataString(relativeUri.ToString());
-
-            if (relativePath.StartsWith("../../../"))
+            using (var textReader = new StreamReader(mapFile))
             {
-                // to many dots, so just return the toPath
-                return toPath;
+                while (!textReader.EndOfStream)
+                {
+                    var fileLine = textReader.ReadLine();
+                    if (fileLine.Length > 0 && fileLine[0] == '#')
+                        continue;
+
+                    var splitLine = fileLine.Split(',');
+                    var sourceFolder = splitLine[0];
+                    var destinationFolder = splitLine[1];
+
+                    //if (sourceFolder.EndsWith("\\"))
+                    //    sourceFolder = sourceFolder.Substring(0, sourceFolder.Length - 1);
+
+                    map.Add(sourceFolder,destinationFolder);
+                }
             }
 
-            return relativePath.Replace('/', Path.DirectorySeparatorChar);
+            return map;
         }
     }
 }
--- src/TFSReplay/TFSReplayOptions.cs
+++ src/TFSReplay/TFSReplayOptions.cs
@@ -13,11 +13,14 @@ namespace TFSReplay
         [Option('c', "changeset", HelpText = "Changeset ID To Replay", Required = true)]
         public int ChangeSetId { get; set; }
 
-        [Option('s', "ServerUrl", HelpText = "TFS Server Url", Required = true)]
+        [Option('s', "ServerUrl", HelpText = "Source TFS Server Url", Required = true)]
         public string ServerUrl { get; set; }
 
         [Option('d', "Destination", HelpText = "Destination Folder that the files will be written to", Required = true)]
         public string Destination { get; set; }
+
+        //[Option('e', "dEstinationUrl", HelpText = "Destination TFS Server Url (if different from source)", Required = false)]
+        //public string DestinationUrl { get; set; }
 
         [Option('b', "BasePath", HelpText = "TFS Server path to replay from (Ex: $/TeamProjectName)", Required = false)]
         public string BaseServerPath { get; set; }
@@ -28,12 +31,18 @@ namespace TFSReplay
         [Option("noprompt", HelpText = "Do not prompt before checking in", Required = false, DefaultValue = false)]
         public bool NoPrompt { get; set; }
 
+        //[Option("merge", HelpText = "Merge items (instead of force)", Required = false, DefaultValue = false)]
+        //public bool MergeItems { get; set; }
+
+        [Option('m', "MapFile", HelpText = "CSV file to remap folders from source to a destination", Required = false)]
+        public string MapFile { get; set; }
+
         [HelpOption]
         public string GetHelp()
         {
             var help = new HelpText
             {
-                Heading = new HeadingInfo("TFSReplay", "0.0.2"),
+                Heading = new HeadingInfo("TFSReplay", "0.0.3"),
                 AdditionalNewLineAfterOption = true,
                 AddDashesToOption = true
             };
